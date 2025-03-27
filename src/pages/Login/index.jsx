207f8e0343133e612e7816caf109c8073e1994c6
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-03-26 23:03:00 -0300

    feat: initial page

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,8 @@
+export function Login() {
+
+  return (
+    <div className="bg-gray-800 min-h-screen flex items-center justify-center">
+      <h1 className="text-white font-bold">Login</h1>
+    </div>
+  )
+}
