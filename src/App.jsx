e44b248e83e179e1d28cf80cafdf648272e46e67
lucasherrerods <lucasherrerosilva.lh@gmail.com>
2025-04-02 21:13:18 -0300

    feat: add toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,23 @@ import Products from './pages/Products'
 import Categories from './pages/Categories'
 import Suppliers from './pages/Suppliers'
 import Movements from './pages/Movements'
+import { ToggleProvider } from './contexts/ToggleSidebar'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Auth />} />
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/categories' element={<Categories />} />
-        <Route path='/suppliers' element={<Suppliers />} />
-        <Route path='/transactions' element={<Movements />} />
-      </Routes>
-    </BrowserRouter>
+    <ToggleProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Auth />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/categories' element={<Categories />} />
+          <Route path='/suppliers' element={<Suppliers />} />
+          <Route path='/transactions' element={<Movements />} />
+        </Routes>
+      </BrowserRouter>
+    </ToggleProvider>
   )
 }
 
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,7 +1,8 @@
 import Logo from '../../assets/logo.png'
-import { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { CircleChevronLeft, LayoutDashboard, Package, Tags, Truck, Shuffle, LogOut } from 'lucide-react'
+import { ToggleSidebar } from '../../contexts/ToggleSidebar'
 
 export default function Sidebar() {
   // Hook para pegar a localização atual da URL
@@ -9,6 +10,8 @@ export default function Sidebar() {
 
   // Verifica se a rota ativa é a atual
   const isActive = (path) => location.pathname === path
+
+  const { isOpen, toggleSidebar } = useContext(ToggleSidebar)
 
   useEffect(() => {
     // Insere o título respectivo da página atual
@@ -26,46 +29,56 @@ export default function Sidebar() {
   }, [location]) // Executa sempre que a URL mudar
 
   return (
-    <nav className="fixed top-0 left-0 h-full bg-[#1A1A27] z-10 w-60 flex flex-col">
+    <nav className={`fixed top-0 left-0 h-full bg-[#1A1A27] z-10 flex flex-col transition-all duration-300 ease-in-out ${isOpen ? 'w-60' : 'w-20'}`}>
       <header className="flex items-center justify-between px-4 py-2 mt-4">
-        <div className='flex gap-3 items-center'>
+        <div className={`flex gap-3 items-center transition-opacity ${isOpen ? 'opacity-100' : 'opacity-0'}`}>
           <img src={Logo} alt="Logo" className='h-6 w-auto rounded-lg' />
-          <h1 className="text-white lowercase font-bold tracking-wide">
+          <h1 className={`text-white lowercase font-bold tracking-wide ${isOpen ? '' : 'hidden'}`}>
             StockFlow
           </h1>
         </div>
-        <CircleChevronLeft size={20} className='text-white font-bold cursor-pointer' />
+        <CircleChevronLeft onClick={toggleSidebar} size={20} className={`text-white font-bold cursor-pointer ${isOpen ? '' : 'rotate-180'}`} />
       </header>
       <div className="flex-1 py-4 mt-10">
         <ul className='text-white text-xs space-y-6 px-4'>
           <li className={`h-10 flex items-center rounded-md transition-all ease-in-out duration-500 ${isActive('/dashboard') ? 'bg-orange-500 hover:opacity-80' : 'hover:bg-white/10'}`}>
             <Link to={'/dashboard'} className='w-full flex items-center gap-3 px-4'>
-              <LayoutDashboard size={16} />
-              <span>Dashboard</span>
+              <div className='min-w-[20px] flex justify-center'>
+                <LayoutDashboard size={16} />
+              </div>
+              <span className={`whitespace-nowrap transition-opacity ${isOpen ? 'opacity-100' : 'opacity-0 w-0'}`}>Dashboard</span>
             </Link>
           </li>
           <li className={`h-10 flex items-center rounded-md transition-all ease-in-out duration-500 ${isActive('/products') ? 'bg-orange-500 hover:opacity-80' : 'hover:bg-white/10'}`}>
             <Link to={'/products'} className='w-full flex items-center gap-3 px-4'>
-              <Package size={16} />
-              <span>Produtos</span>
+              <div className='min-w-[20px] flex justify-center'>
+                <Package size={16} />
+              </div>
+              <span className={`whitespace-nowrap transition-opacity ${isOpen ? 'opacity-100' : 'opacity-0 w-0'}`}>Produtos</span>
             </Link>
           </li>
           <li className={`h-10 flex items-center rounded-md transition-all ease-in-out duration-500 ${isActive('/categories') ? 'bg-orange-500 hover:opacity-80' : 'hover:bg-white/10'}`}>
             <Link to={'/categories'} className='w-full flex items-center gap-3 px-4'>
-              <Tags size={16} />
-              <span>Categorias</span>
+              <div className='min-w-[20px] flex justify-center'>
+                <Tags size={16} />
+              </div>
+              <span className={`whitespace-nowrap transition-opacity ${isOpen ? 'opacity-100' : 'opacity-0 w-0'}`}>Categorias</span>
             </Link>
           </li>
           <li className={`h-10 flex items-center rounded-md transition-all ease-in-out duration-500 ${isActive('/suppliers') ? 'bg-orange-500 hover:opacity-80' : 'hover:bg-white/10'}`}>
             <Link to={'/suppliers'} className='w-full flex items-center gap-3 px-4'>
-              <Truck size={16} />
-              <span>Fornecedores</span>
+              <div className='min-w-[20px] flex justify-center'>
+                <Truck size={16} />
+              </div>
+              <span className={`whitespace-nowrap transition-opacity ${isOpen ? 'opacity-100' : 'opacity-0 w-0'}`}>Fornecedores</span>
             </Link>
           </li>
           <li className={`h-10 flex items-center rounded-md transition-all ease-in-out duration-500 ${isActive('/transactions') ? 'bg-orange-500 hover:opacity-80' : 'hover:bg-white/10'}`}>
             <Link to={'/transactions'} className='w-full flex items-center gap-3 px-4'>
-              <Shuffle size={16} />
-              <span>Movimentações</span>
+              <div className='min-w-[20px] flex justify-center'>
+                <Shuffle size={16} />
+              </div>
+              <span className={`whitespace-nowrap transition-opacity ${isOpen ? 'opacity-100' : 'opacity-0 w-0'}`}>Movimentações</span>
             </Link>
           </li>
         </ul>
@@ -73,8 +86,10 @@ export default function Sidebar() {
       <div className='border-t border-white/20 py-3 px-4 mt-auto'>
         <Link to={'/'}>
           <button onClick={() => localStorage.removeItem('token')} className='w-full h-10 flex items-center gap-3 text-white text-xs rounded-md px-4 transation-all ease-in-out duration-500 hover:bg-white/10 cursor-pointer'>
-            <LogOut size={16} />
-            <span>Logout</span>
+            <div className='min-w-[20px] flex justify-center'>
+              <LogOut size={16} />
+            </div>
+            <span className={`whitespace-nowrap transition-opacity ${isOpen ? 'opacity-100' : 'opacity-0 w-0'}`}>Sair</span>
           </button>
         </Link>
       </div>
